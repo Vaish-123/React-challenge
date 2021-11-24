fc702471a5b0b01accb1e386308b38d206e6d609
Vaish-123 <vaishnavnampoothiri@gamil.com>
2021-11-24 23:16:42 +0530

    mapping

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import Counter from './components/counter';
+import Employee from "./components/maping";
+
 function App() {
-  const [count, setCount] = useState(0);
-  const addCount = () => {
-    setCount(count + 1)
-  }
-  let obj={
-    title:'1st counter',
-    c:count
-  }
+  let emp = [{ name: 'Vvn', age: 21 }, { name: 'abc', age: 12 }]
   return (
     <div>
       <h1>Hello user</h1>
-      <Counter {...obj}/>
-      <Counter title='2nd counter' c={count}/>
-      <button onClick={addCount}>Add</button>
+      {
+        emp.map((obj, index) => 
+          <Employee key={index} {...obj} />
+        )
+      }
     </div>
   );
 }
