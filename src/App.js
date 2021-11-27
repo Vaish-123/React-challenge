148c657027e2ad012ef5a5eafef8b804f01a1435
Vaish-123 <vaishnavnampoothiri@gamil.com>
2021-11-28 01:46:00 +0530

    Basic ToDo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import Usf from './components/useEffect'
-import { useState } from 'react';
+import { useState } from 'react/cjs/react.development';
+import './App.css'
 function App() {
-  const [state,setState]=useState(false)
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([])
+  const [Delete, setDelete] = useState([])
+  const del = (index) => {
+    var temp = toDos.filter((d, i) => {
+      if (index !== i)
+        return d
+      setDelete([...Delete, d])
+    })
+    setToDos(temp)
+  }
+  const chk = (index) => {
+    var temp = toDos.map((obj, i) => {
+      if (index === i)
+        return { text: obj.text, status: !obj.status }
+      return obj
+    })
+    setToDos(temp)
+  }
   return (
-    <div>
-      <h1>Hello User</h1>
-      <h1 onClick={()=>setState(!state)}>Show/Hide</h1>
-      {state?<Usf/>:null}
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." id='abc' />
+        <i onClick={() => { setToDos([...toDos, { text: toDo, status: false }]); setToDo('') }} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((value, index) => {
+            return (<div className="todo">
+              <div className="left">
+                <input onClick={() => { chk(index) }} type="checkbox" checked={value.status} />
+                {
+                  value.status ? <p id='chk'>{value.text}</p> : <p>{value.text}</p>
+                }
+              </div>
+              <div className="right">
+                <i onClick={() => { del(index) }} className="fas fa-trash-alt"></i>
+              </div>
+            </div>
+            )
+          })
+        }
+      </div>
+      <div>
+        <ul>
+          {
+            Delete.map(((data)=>
+            (
+              <li>{data.text}</li>
+            )
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
